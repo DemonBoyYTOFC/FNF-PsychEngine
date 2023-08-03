3b033a4a1f6bbb2a73a30fdc5f5fda682715ee60
S4DBoy_x <124374135+DemonBoyYTOFC@users.noreply.github.com>
2023-08-03 09:49:33 -0300

    Update PlayState.hx

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -2244,7 +2244,7 @@ class PlayState extends MusicBeatState
 	public function updateScore(miss:Bool = false)
 	{
 		scoreTxt.text = 'Score: ' + songScore
-		+ ' | Port by Glauber_BFSoulEvilYT | Misses: ' + songMisses
+		+ ' | port by glauber | Misses: ' + songMisses
 		+ ' | Misses: ' + songMisses
 		+ ' | Rating: ' + ratingName
 		+ (ratingName != '?' ? ' (${Highscore.floorDecimal(ratingPercent * 100, 2)}%) - $ratingFC' : '');
